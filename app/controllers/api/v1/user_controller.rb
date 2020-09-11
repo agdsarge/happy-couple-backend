50db922cc019c1cd75f72ecfb67f06a8cf35036c
agdsarge <agdsargeant@gmail.com>
2020-09-11 15:36:53 -0400

    some testing stuff

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -5,7 +5,8 @@ class Api::V1::UserController < ApplicationController
         # byebug
         # @user = User.find(@id)
         # my_wedding = Wedding.joins(:users).where(users: {id: @id}) 
-        render json: {weddingDate: 'Thu, 31 Dec 2020 18:30:00 UTC +00:00'}
+        render json: {weddingDate: 'Thu, 31 Dec 2020 18:30:00 UTC +00:00'} 
+        #render json: {weddingDate: 'Fri, 18 Sep 2020 18:30:00 UTC +00:00'} 
     end
 
 end
