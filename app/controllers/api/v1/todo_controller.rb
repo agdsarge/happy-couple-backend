130bc45f059b23b0e9e327d654e7cbb8eb10faff
epittab <ep7m@me.com>
2020-09-22 16:55:54 -0400

    added todo route and controller

---
+++ app/controllers/api/v1/todo_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::TodoController < ApplicationController
+    before_action :authorize_request
+    def update
+        byebug
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
             post '/register', to: 'auth#register'
             get '/date', to: 'user#get_wedding_date'
             post '/setup', to: 'wedding#create_new_wedding_from_wizard'
+            post '/todos', to: 'todos#update'
         end
     end
 end
