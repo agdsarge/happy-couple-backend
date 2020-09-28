644ff7caaec15383aeafb59c59691842b9c72bd5
epittab <ep7m@me.com>
2020-09-28 13:08:05 -0400

    todo controller adapted for wedding_id from front end

--- app/controllers/api/v1/todo_controller.rb
+++ app/controllers/api/v1/todo_controller.rb
@@ -1,21 +1,30 @@
 class Api::V1::TodoController < ApplicationController
+
     before_action :authorize_request
+    
     def update
-        byebug
+        returnList = []
         list = params[:todoList]
         list.each do |todo| 
-            wedding_todo = Todo.where("wedding_id = ? AND task_name = ?", params[:wedding_id], todo.task_name)
-            if wedding_todo
-                if (wedding_todo.isCompleted != todo.isCompleted)
-                    wedding_todo.update(isCompleted: todo.isCompleted)
+            wedding_todo = ToDo.where("wedding_id = ? AND todo_name = ?", params[:wedding_id], todo[:task])
+            
+            if (wedding_todo.size > 0 )
+                
+                if (wedding_todo[0][:isCompleted] != todo[:isCompleted])
+                    wedding_todo.update(isCompleted: todo[:isCompleted])
+                    returnList << wedding_todo[0]
+                else
+                    returnList << wedding_todo[0]
                 end
+
             else
-                new_todo = Todo.new(wedding_id: params[:wedding_id], task_name: todo.task_name, isCompleted: todo.isCompleted )
+                new_todo = ToDo.new(wedding_id: params[:wedding_id].to_i, todo_name: todo[:task], isCompleted: todo[:isCompleted] )
                 new_todo.save()
+                returnList << new_todo
             end
         end 
 
-        render json: {message: "success"}, status: :ok
+        render json: {message: "success", list: returnList}, status: :ok
     end
 
 end
